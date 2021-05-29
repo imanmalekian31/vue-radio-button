aeda8e7a1ead995847533702a271f1012de4fedf
iman malekian <imanmalekian31@gmail.com>
2021-05-29 22:45:14 +0430

    Add exmaple five

---
+++ src/examples/ExampleFive.vue
@@ -0,0 +1,70 @@
+<template>
+  <VueRadioButton
+    v-model="selectedButton"
+    :options="buttons"
+    color="#d3edfe"
+    width="150"
+    height="80"
+  >
+    <template #default="{ props }">
+      <div class="vueRadioButton">
+        <img width="40" height="40" :src="props.icon" class="icon" />
+        <div class="title">
+          {{ props.title }}
+        </div>
+      </div>
+    </template>
+  </VueRadioButton>
+</template>
+
+<script>
+import VueRadioButton from "../components/VueRadioButton";
+
+export default {
+  components: { VueRadioButton },
+  data: () => ({
+    selectedButton: "",
+    buttons: [
+      {
+        id: 1,
+        icon: require("../assets/logo.png"),
+        title: "Vue",
+      },
+      {
+        id: 2,
+        icon: require("../assets/nuxt.svg"),
+        title: "Nuxt",
+      },
+      {
+        id: 3,
+        icon: require("../assets/vite.svg"),
+        title: "Vite",
+      },
+      {
+        id: 4,
+        icon: require("../assets/bootstrap-vue.svg"),
+        title: "Bootstrap",
+      },
+      {
+        id: 5,
+        icon: require("../assets/vuetify.svg"),
+        title: "Vuetify",
+      },
+    ],
+  }),
+};
+</script>
+<style scoped>
+.vueRadioButton {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.icon {
+  object-fit: contain;
+}
+</style>
