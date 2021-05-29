f12008677aeb18f89eaca81875bd5c8f7702e1ad
iman malekian <imanmalekian31@gmail.com>
2021-05-29 21:51:01 +0430

    Add some examples

---
+++ src/examples/ExampleTwo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <VueRadioButton
+      v-model="selectedButton"
+      :options="buttons"
+      color="#f8a1155e"
+    >
+      <template #default="{ props }">
+        <div class="vueRadioButton">
+          <img width="40" height="40" :src="props.icon" class="icon" />
+        </div>
+      </template>
+    </VueRadioButton>
+  </div>
+</template>
+
+<script>
+import VueRadioButton from "../components/VueRadioButton";
+
+export default {
+  components: { VueRadioButton },
+  data: () => ({
+    selectedButton: "",
+    buttons: [
+      {
+        id: 1,
+        icon: require("../assets/logo.png"),
+        title: "Vue",
+      },
+      {
+        id: 2,
+        icon: require("../assets/nuxt.svg"),
+        title: "Nuxt",
+      },
+      {
+        id: 3,
+        icon: require("../assets/vite.svg"),
+        title: "Vite",
+      },
+      {
+        id: 4,
+        icon: require("../assets/bootstrap-vue.svg"),
+        title: "Bootstrap",
+      },
+      {
+        id: 5,
+        icon: require("../assets/vuetify.svg"),
+        title: "Vuetify",
+      },
+    ],
+  }),
+};
+</script>
+<style scoped>
+.vueRadioButton {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.icon {
+  object-fit: contain;
+}
+</style>
---
+++ src/examples/index.js
@@ -0,0 +1,4 @@
+export { default as ExampleOne } from "./ExampleOne";
+export { default as ExampleTwo } from "./ExampleTwo";
+export { default as ExampleThree } from "./ExampleThree";
+export { default as ExampleFour } from "./ExampleFour";
