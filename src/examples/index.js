e7ded9522b019e16fd7de67c85ab6632c5cbbd56
iman malekian <imanmalekian31@gmail.com>
2021-05-29 22:45:28 +0430

    Add example five

--- src/examples/index.js
+++ src/examples/index.js
@@ -2,3 +2,4 @@ export { default as ExampleOne } from "./ExampleOne";
 export { default as ExampleTwo } from "./ExampleTwo";
 export { default as ExampleThree } from "./ExampleThree";
 export { default as ExampleFour } from "./ExampleFour";
+export { default as ExampleFive } from "./ExampleFive";
