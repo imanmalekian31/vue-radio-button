5856c62b57448b20a3a115b34463de2f16ef1183
iman malekian <imanmalekian31@gmail.com>
2021-06-01 18:29:23 +0430

    refactor(install): :recycle: Improve install plugin

--- src/install.js
+++ src/install.js
@@ -1,13 +1,27 @@
 import VueRadioButton from "./components/VueRadioButton";
 
-const VueRadioButtonInstall = {
-  install(Vue) {
-    Vue.component("vue-radio-button", VueRadioButton);
-  },
-};
-
-if (typeof window !== "undefined" && window.Vue) {
-  window.Vue.use(VueRadioButtonInstall);
+// Declare install function executed by Vue.use()
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("vue-radio-button", VueRadioButton);
 }
 
-export default VueRadioButtonInstall;
+// Create module definition for Vue.use()
+const plugin = {
+  install,
+};
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+// To allow use as module (npm/webpack/etc.) export component
+export default VueRadioButton;
