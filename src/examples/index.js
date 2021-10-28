5cc8a1ce5856b9753b33a57b6d0c5635f0632606
iman malekian <imanmalekian31@gmail.com>
2021-10-28 12:01:59 +0330

    refactor(examples): :fire: Remove examples

--- src/examples/index.js
+++
@@ -1,6 +0,0 @@
-export { default as ExampleOne } from "./ExampleOne";
-export { default as ExampleTwo } from "./ExampleTwo";
-export { default as ExampleThree } from "./ExampleThree";
-export { default as ExampleFour } from "./ExampleFour";
-export { default as ExampleFive } from "./ExampleFive";
-export { default as ExampleSix } from "./ExampleSix";
